c7fe5e90f62b2cc8768a95c8547aa4a08d51be69
Henry Han <48421873+chronushan@users.noreply.github.com>
2024-05-13 17:23:04 +0900

    Add function to create grid from prompt value

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-for (let i = 0; i < 16; i++) {
-	const rowDiv = document.createElement("div");
-	rowDiv.className = `rowDiv${i}`;
-	document.querySelector(".container").appendChild(rowDiv);
+// for (let i = 0; i < 16; i++) {
+// 	const rowDiv = document.createElement("div");
+// 	rowDiv.className = `rowDiv${i}`;
+// 	document.querySelector(".container").appendChild(rowDiv);
 
-	for (let j = 0; j < 16; j++) {
-		//default display of div element is block, which makes the created indvDiv go down vertically instead of horizontally
-		var indvDiv = document.createElement("div");
-		document.querySelector(`.rowDiv${i}`).appendChild(indvDiv);
-		indvDiv.style.cssText = "border: solid black; padding: 15px";
-		indvDiv.addEventListener("mouseenter", (event) => {
-			event.target.style.backgroundColor = "red";
-		});
+// 	for (let j = 0; j < 16; j++) {
+// 		//default display of div element is block, which makes the created indvDiv go down vertically instead of horizontally
+// 		var indvDiv = document.createElement("div");
+// 		document.querySelector(`.rowDiv${i}`).appendChild(indvDiv);
+// 		indvDiv.style.cssText = "border: solid black; padding: 15px";
+// 		indvDiv.addEventListener("mouseenter", (event) => {
+// 			event.target.style.backgroundColor = "red";
+// 		});
+// 	}
+// }
+
+function gridMaker(dimensions = 16) {
+	for (let i = 0; i < dimensions; i++) {
+		const rowDiv = document.createElement("div");
+		rowDiv.className = `rowDiv${i}`;
+		document.querySelector(".container").appendChild(rowDiv);
+
+		for (let j = 0; j < dimensions; j++) {
+			//default display of div element is block, which makes the created indvDiv go down vertically instead of horizontally
+			var indvDiv = document.createElement("div");
+			document.querySelector(`.rowDiv${i}`).appendChild(indvDiv);
+			indvDiv.style.cssText =
+				"display: flex;  border: 1px solid black; padding: 5px";
+			indvDiv.addEventListener("mouseenter", (event) => {
+				event.target.style.backgroundColor = "red";
+			});
+		}
 	}
 }
 
@@ -30,3 +49,5 @@ btn.addEventListener("click", () => {
 		}
 	}
 });
+
+gridMaker();
