2ac6e0f2458c1f81dac1878533396b7774d16494
Henry Han <48421873+chronushan@users.noreply.github.com>
2024-05-13 14:54:44 +0900

    Add event listner for mouse over

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ for (let i = 0; i < 16; i++) {
 	const rowDiv = document.createElement("div");
 	rowDiv.className = `rowDiv${i}`;
 	document.querySelector(".container").appendChild(rowDiv);
-	rowDiv.style.border = "solid red";
 
 	for (let j = 0; j < 16; j++) {
 		//default display of div element is block, which makes the created indvDiv go down vertically instead of horizontally
-		const indvDiv = document.createElement("div");
+		var indvDiv = document.createElement("div");
 		document.querySelector(`.rowDiv${i}`).appendChild(indvDiv);
 		indvDiv.style.cssText = "border: solid black; padding: 10px";
-		indvDiv.textContent = j + 1;
+		indvDiv.addEventListener("mouseenter", (event) => {
+			event.target.style.backgroundColor = "red";
+		});
 	}
 }
