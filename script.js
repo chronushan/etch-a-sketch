aa85f21964be4cd3eaf260b0c1926374ebaff7c7
Henry Han <48421873+chronushan@users.noreply.github.com>
2024-05-13 16:50:15 +0900

    Edit prompt for grid

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ for (let i = 0; i < 16; i++) {
 		//default display of div element is block, which makes the created indvDiv go down vertically instead of horizontally
 		var indvDiv = document.createElement("div");
 		document.querySelector(`.rowDiv${i}`).appendChild(indvDiv);
-		indvDiv.style.cssText = "border: solid black; padding: 10px";
+		indvDiv.style.cssText = "border: solid black; padding: 15px";
 		indvDiv.addEventListener("mouseenter", (event) => {
 			event.target.style.backgroundColor = "red";
 		});
@@ -17,25 +17,13 @@ for (let i = 0; i < 16; i++) {
 const btn = document.querySelector(".btn");
 
 btn.addEventListener("click", () => {
-	let gridRows = Number(prompt("Enter number of rows (1~100): "));
+	let gridNum = Number(prompt("Enter number for grid (1~100): "));
 	while (true) {
-		if (isNaN(gridRows)) {
-			gridRows = Number(prompt("Please only enter number(s) for rows: "));
-		} else if (gridRows > 100 || gridRows < 1) {
-			gridRows = Number(
-				prompt("Please enter a number within the range for rows (1~100): ")
-			);
-		} else {
-			break;
-		}
-	}
-	let gridCol = Number(prompt("Enter number of columns (1~100): "));
-	while (true) {
-		if (isNaN(gridCol)) {
-			gridCol = Number(prompt("Please only enter number(s) for columns: "));
-		} else if (gridCol > 100 || gridCol < 1) {
-			gridCol = Number(
-				prompt("Please enter a number within the range for columns (1~100): ")
+		if (isNaN(gridNum)) {
+			gridNum = Number(prompt("Please only enter number(s): "));
+		} else if (gridNum > 100 || gridNum < 1) {
+			gridNum = Number(
+				prompt("Please enter a number within the range (1~100): ")
 			);
 		} else {
 			break;
