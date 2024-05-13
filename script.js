5c73077d15c6d09a58eb7d2b73b0eb508030f9b8
Henry Han <48421873+chronushan@users.noreply.github.com>
2024-05-13 12:23:31 +0900

    Add 16x16 grid using flex

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-let sketchDiv = document.createElement("div");
 for (let i = 0; i < 16; i++) {
-	document.querySelector(".container").appendChild(sketchDiv);
+	const rowDiv = document.createElement("div");
+	rowDiv.className = `rowDiv${i}`;
+	document.querySelector(".container").appendChild(rowDiv);
+	rowDiv.style.border = "solid red";
+
+	for (let j = 0; j < 16; j++) {
+		const indvDiv = document.createElement("div");
+		document.querySelector(`.rowDiv${i}`).appendChild(indvDiv);
+		indvDiv.style.cssText = "border: solid black; padding: 10px";
+		indvDiv.textContent = j + 1;
+	}
 }
