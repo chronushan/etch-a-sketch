dd6308b503159ab29be8b5990d9e84b9e49dc2e0
Henry Han <48421873+chronushan@users.noreply.github.com>
2024-05-13 12:29:58 +0900

    Add comment explaining the created divs

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ for (let i = 0; i < 16; i++) {
 	rowDiv.style.border = "solid red";
 
 	for (let j = 0; j < 16; j++) {
+		//default display of div element is block, which makes the created indvDiv go down vertically instead of horizontally
 		const indvDiv = document.createElement("div");
 		document.querySelector(`.rowDiv${i}`).appendChild(indvDiv);
 		indvDiv.style.cssText = "border: solid black; padding: 10px";
