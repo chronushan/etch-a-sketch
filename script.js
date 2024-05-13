8ec153d6b9324e5cfaa35ea1cdc09f684006249c
Henry Han <48421873+chronushan@users.noreply.github.com>
2024-05-13 17:50:28 +0900

    Add column class and borders for rows

--- script.js
+++ script.js
@@ -1,34 +1,19 @@
-// for (let i = 0; i < 16; i++) {
-// 	const rowDiv = document.createElement("div");
-// 	rowDiv.className = `rowDiv${i}`;
-// 	document.querySelector(".container").appendChild(rowDiv);
-
-// 	for (let j = 0; j < 16; j++) {
-// 		//default display of div element is block, which makes the created indvDiv go down vertically instead of horizontally
-// 		var indvDiv = document.createElement("div");
-// 		document.querySelector(`.rowDiv${i}`).appendChild(indvDiv);
-// 		indvDiv.style.cssText = "border: solid black; padding: 15px";
-// 		indvDiv.addEventListener("mouseenter", (event) => {
-// 			event.target.style.backgroundColor = "red";
-// 		});
-// 	}
-// }
-
 function gridMaker(dimensions = 16) {
 	for (let i = 0; i < dimensions; i++) {
 		const rowDiv = document.createElement("div");
-		rowDiv.className = `rowDiv${i}`;
+		rowDiv.className = `rowDiv${i} rows`;
 		document.querySelector(".container").appendChild(rowDiv);
 
 		for (let j = 0; j < dimensions; j++) {
 			//default display of div element is block, which makes the created indvDiv go down vertically instead of horizontally
 			var indvDiv = document.createElement("div");
+			indvDiv.className = "columns";
 			document.querySelector(`.rowDiv${i}`).appendChild(indvDiv);
-			indvDiv.style.cssText =
-				"display: flex; border: 1px solid black; padding: 5px";
+			indvDiv.style.cssText = " border: 1px solid black; padding: 5px";
 			indvDiv.addEventListener("mouseenter", (event) => {
 				event.target.style.backgroundColor = "red";
 			});
+			// indvDiv.textContent = j;
 		}
 	}
 }
@@ -50,4 +35,4 @@ btn.addEventListener("click", () => {
 	}
 });
 
-gridMaker(10);
+gridMaker(50);
